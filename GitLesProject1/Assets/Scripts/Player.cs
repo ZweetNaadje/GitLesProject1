e6583164731f5508d66f16a9a47edb131dc8b60a
Bryan <dinsbachbryan@gmail.com>
2023-02-18 13:56:00 +0100

    Added player script

---
+++ GitLesProject1/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        //Movement code here...
+    }
+}
